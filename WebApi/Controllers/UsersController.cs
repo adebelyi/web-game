3ac0a22d480903b3c7e7687dd08c2603d91944d8
Alexander Debelyi <debelyy@yandex.ru>
2019-03-06 22:45:23 +0500

    implemented: get, head, post, delete

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -1,22 +1,79 @@
 using System;
+using System.Linq;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Models;
+using WebGame.Domain;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     public class UsersController : Controller
     {
-        [HttpGet("{userId}")]
+        private readonly IUserRepository userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        [HttpGet("{userId}", Name = nameof(GetUserById))]
+        [HttpHead("{userId}")]
         public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
         {
-            throw new NotImplementedException();
+            var userFromRepository = userRepository.FindById(userId);
+
+            if (userFromRepository == null)
+            {
+                return NotFound();
+            }
+
+            var userDto = Mapper.Map<UserDto>(userFromRepository);
+
+            return Ok(userDto);
         }
 
         [HttpPost]
-        public IActionResult CreateUser([FromBody] object user)
+        public IActionResult CreateUser([FromBody] CreateUserDTO user)
         {
-            throw new NotImplementedException();
+            if (user == null)
+            {
+                return BadRequest();
+            }
+
+            if (user.Login == null || !user.Login.All(l => char.IsDigit(l) || char.IsLetter(l)))
+            {
+                ModelState.AddModelError(nameof(CreateUserDTO.Login), "Логин должен состоять только из цифр и букв.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return UnprocessableEntity(ModelState);
+            }
+
+            var createdUserEntity = Mapper.Map<UserEntity>(user);
+            createdUserEntity = userRepository.Insert(createdUserEntity);
+
+            return CreatedAtRoute(
+                nameof(GetUserById),
+                new {userId = createdUserEntity.Id},
+                createdUserEntity.Id
+            );
+        }
+
+        [HttpDelete("{userId}")]
+        public IActionResult CreateUser([FromRoute] Guid userId)
+        {
+            var user = userRepository.FindById(userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            userRepository.Delete(userId);
+
+            return NoContent();
         }
     }
 }
